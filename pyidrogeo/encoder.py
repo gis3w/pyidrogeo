8b1c50f68f42be787360f696e0672f4145c89fd6
Andrea Antonello <andrea.antonello@gmail.com>
2024-11-26 12:09:50 +0100

    fixes in encoder + update version

--- pyidrogeo/encoder.py
+++ pyidrogeo/encoder.py
@@ -18,16 +18,22 @@ def fixValues(data:dict):
     for key, value in data.items():
         # field "gruppi_elementi_danni" has to be a list of strings
         if key == 'gruppi_elementi_danni':
-            data[key] = [str(value)]
+            string2List(data, key, value)
         # field "Compila danni" has to be a boolean
         elif key == 'compila_danni':
             data[key] = bool(value)
         # field "Gruppi elementi danni (danni di primo livello)" has to be a list of strings
         elif key == 'gruppi_elementi_danni_danni_di_primo_livello':
-            data[key] = [str(value)]
+            string2List(data, key, value)
         # field "Metodo" has to be a list of strings
         elif key == 'metodo':
-            data[key] = [str(value)]
+            string2List(data, key, value)
 
+    return data 
 
-    return data    
+def string2List(data, key, value):
+    if value:
+        v = str(value)
+        data[key] = [v]
+    else:
+        data[key] = []   
